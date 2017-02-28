bceec5add86fa18acd89b1fe994f60e28feb2f12
rachelmiller <rkm2314@tc.columbia.edu>
2017-02-27 19:40:57 -0500

    Added display text view and a Person Class

--- app/src/main/java/rachelmiller/class07/MainActivity.java
+++ app/src/main/java/rachelmiller/class07/MainActivity.java
@@ -2,12 +2,18 @@ package rachelmiller.class07;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView displayText; // found the display text - whatever the TextView is you should automatically
+
+    // find it, name it and declare it here and initialize in onCreate
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        displayText = (TextView) findViewById(R.id.display_text); // initialized the mre text
     }
 }
---
+++ app/src/main/java/rachelmiller/class07/Person.java
@@ -0,0 +1,8 @@
+package rachelmiller.class07;
+
+/**
+ * Created by rachelmiller on 2/27/17.
+ */
+
+public class Person {
+}
